a78de9ce2e85d120e2dcd239968bc6236ec494f5
YanHaoChen <coolking29@gmail.com>
2016-06-28 22:24:26 +0800

    ImageDistinguish:Add output information.

--- ImageDistinguish.py
+++ ImageDistinguish.py
@@ -1,6 +1,6 @@
 import cv2
-
-img = cv2.imread('Distinguish.jpg')
+imageName = "XXX.jpg"
+img = cv2.imread(imageName)
 
 white = 0.0
 black = 0.0
@@ -8,15 +8,19 @@ allPixels = img.shape[0] * img.shape[1]
 for height in xrange(img.shape[0]):
 	for width in xrange(img.shape[1]):
 		(b,g,r)=img[height,width]
-		if b > 240 and g >240 and r > 240:
-			img[height,width]=(255,0,0)
-			white+=1
-		elif b < 50 and g < 50 and r <50 :
+		if b < 50 and g < 50 and r <50:
 			img[height,width]=(0,255,0)
 			black+=1
+		elif b > 240 and g >240 and r > 240:
+			img[height,width]=(255,0,0)
+			white+=1
 
+print imageName
 print "All:%d" % allPixels
 print "[r>240,g>240,b>240]White:%.2f %% " % (float(white)/float(allPixels)*100)
 print "[r< 50,g< 50,b< 50]Black:%.2f %% " % (float(black)/float(allPixels)*100)
-print "view Judged.jpg(white->blue,black->green)"
-cv2.imwrite('Judged.jpg',img)
+print "view Distinguished.jpg(white->blue,black->green)"
+
+outputFileName = "%s-Distinguished.jpg" % imageName
+
+cv2.imwrite(outputFileName,img)
