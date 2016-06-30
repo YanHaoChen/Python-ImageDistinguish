c764455c2a891687333a441b9fc424d772aa2592
YanHaoChen <coolking29@gmail.com>
2016-06-30 21:41:31 +0800

    Update README.md and make it to be useful.

--- ImageDistinguish.py
+++ ImageDistinguish.py
@@ -1,26 +1,65 @@
+import os
+import sys
 import cv2
-imageName = "XXX.jpg"
-img = cv2.imread(imageName)
+import getopt
 
-white = 0.0
-black = 0.0
-allPixels = img.shape[0] * img.shape[1]
-for height in xrange(img.shape[0]):
-	for width in xrange(img.shape[1]):
-		(b,g,r)=img[height,width]
-		if b < 50 and g < 50 and r <50:
-			img[height,width]=(0,255,0)
-			black+=1
-		elif b > 240 and g >240 and r > 240:
-			img[height,width]=(255,0,0)
-			white+=1
+def usage():
+	print "Image Distinguish(white and black)"
+	print "-a          :Takes all of pictures from the same directory with the project to distinguish."
+	print "-n [picture]:Takes the picture selected to distinguish.\n"
+	print "example:"
+	print "python ImageDistinguish.py -a"
+	print "python ImageDistinguish.py -n xxx.jpg"
+	sys.exit(0)
 
-print imageName
-print "All:%d" % allPixels
-print "[r>240,g>240,b>240]White:%.2f %% " % (float(white)/float(allPixels)*100)
-print "[r< 50,g< 50,b< 50]Black:%.2f %% " % (float(black)/float(allPixels)*100)
-print "view Distinguished.jpg(white->blue,black->green)"
+def main(picName,allPicture):
+	if allPicture:
+		picName = [file for file in os.listdir('.') if ".jpg" in file or ".png" in file]
 
-outputFileName = "%s-Distinguished.jpg" % imageName
+	with open('result.txt','w') as f:
+		for pic in picName:
+			try:
+				img = cv2.imread(pic)
+				white = 0.0
+				black = 0.0
+				allPixels = img.shape[0] * img.shape[1]
+				for height in xrange(img.shape[0]):
+					for width in xrange(img.shape[1]):
+						(b, g, r) = img[height, width]
+						if b < 50 and g < 50 and r < 50:
+							img[height,width] = (0,255,0)
+							black+=1
+						elif b > 240 and g > 240 and r > 240:
+							img[height,width] = (255,0,0)
+							white+=1
 
-cv2.imwrite(outputFileName,img)
+				print >>f,"image: %s" % str(pic)
+				print >>f,"[r>240,g>240,b>240]White:%.2f %% " % (float(white)/float(allPixels)*100)
+				print >>f,"[r< 50,g< 50,b< 50]Black:%.2f %% " % (float(black)/float(allPixels)*100)
+				outputFileName = "%s-Distinguished.%s" % (str(pic).split('.')[0],str(pic).split('.')[-1])
+				cv2.imwrite(outputFileName,img)
+				print >>f,"View the %s.(white->blue,black->green)\n" % outputFileName
+				print "%s is finished." % str(pic)
+			except:
+				print "There are some problems with %s." % pic
+
+if not len(sys.argv[1:]):
+	usage()
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"an:",["all","name"])
+except getopt.GetoptError as err:
+	print str(err)
+
+allPicture = False
+picName = []
+
+for o,v in opts:
+	if o in ("-a","--all"):
+		allPicture = True
+	elif o in ("-n","--name"):
+		picName.append(v)
+	else:
+		pass
+
+main(picName,allPicture)
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Python-Image Distinguish(white and black)
 
 The project is used to distinguish every pixels between color difference of white and black from a picture. Moveover, it can calculate the percentage of the white and the black from it. 
+
+### Usage
+
+```shell
+-a          :Takes all of pictures from the same directory with the project to distinguish.
+-n [picture]:Takes the picture ofselected to distinguish.
+
+example:
+$ python ImageDistinguish.py -a
+$ python ImageDistinguish.py -n xxx.jpg
+```
